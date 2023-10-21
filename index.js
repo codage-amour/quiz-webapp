a39efb9f832283e5114666f25b3e16528ff238a5
codage-amour <palak22154041@akgec.ac.in>
2023-10-21 19:41:20 +0530

    css added

--- index.js
+++ index.js
@@ -8,78 +8,78 @@ const questions = [
             { text: "Diana", right: false }
         ]
     },
-    {
-        ques: "Who was the goddess of love and beauty?",
-        ans: [
-            { text: "Aphrodite", right: true },
-            { text: "Iris", right: false },
-            { text: "Zeus", right: false },
-            { text: "Ares", right: false }
-        ]
-    },
-    {
-        ques: "Who is the Greek god of war?",
-        ans: [
-            { text: "Cratos", right: false },
-            { text: "Cupid", right: false },
-            { text: "Ares", right: true },
-            { text: "Iris", right: false }
-        ]
-    },
-    {
-        ques: "Which god had winged sandals?",
-        ans: [
-            { text: "Hermes", right: true },
-            { text: "Melinoe", right: false },
-            { text: "Phobus", right: false },
-            { text: "Tyche", right: false }
-        ]
-    },
-    {
-        ques: "Who was the Greek god of the Underworld?",
-        ans: [
-            { text: "Hades", right: true },
-            { text: "Diana", right: false },
-            { text: "Phobus", right: false },
-            { text: "Mars", right: false }
-        ]
-    },
-    {
-        ques: "Who was the Greek goddess of wisdom?",
-        ans: [
-            { text: "Melinoe", right: false },
-            { text: "Cratos", right: false },
-            { text: "Athena", right: true },
-            { text: "Iris", right: false }
-        ]
-    },
-    {
-        ques: "Who was the muse of love poetry?",
-        ans: [
-            { text: "Tyche", right: false },
-            { text: "Cupid", right: false },
-            { text: "Mars", right: false },
-            { text: "Erato", right: true }
-        ]
-    },
-    {
-        ques: "Who was the muse of comedy?",
-        ans: [
-            { text: "Thalia", right: true },
-            { text: "Diana", right: false },
-            { text: "Melinoe", right: false },
-            { text: "Tyche", right: false }
-        ]
-    },
-    {
-        ques: "Who was the Greek god of light and the sun?",
-        ans: [
-            { text: "Diana", right: false },
-            { text: "Phobus", right: false },
-            { text: "Apollo", right: true },
-            { text: "Cratos", right: false }
-        ]
-    },
+    // {
+    //     ques: "Who was the goddess of love and beauty?",
+    //     ans: [
+    //         { text: "Aphrodite", right: true },
+    //         { text: "Iris", right: false },
+    //         { text: "Zeus", right: false },
+    //         { text: "Ares", right: false }
+    //     ]
+    // },
+    // {
+    //     ques: "Who is the Greek god of war?",
+    //     ans: [
+    //         { text: "Cratos", right: false },
+    //         { text: "Cupid", right: false },
+    //         { text: "Ares", right: true },
+    //         { text: "Iris", right: false }
+    //     ]
+    // },
+    // {
+    //     ques: "Which god had winged sandals?",
+    //     ans: [
+    //         { text: "Hermes", right: true },
+    //         { text: "Melinoe", right: false },
+    //         { text: "Phobus", right: false },
+    //         { text: "Tyche", right: false }
+    //     ]
+    // },
+    // {
+    //     ques: "Who was the Greek god of the Underworld?",
+    //     ans: [
+    //         { text: "Hades", right: true },
+    //         { text: "Diana", right: false },
+    //         { text: "Phobus", right: false },
+    //         { text: "Mars", right: false }
+    //     ]
+    // },
+    // {
+    //     ques: "Who was the Greek goddess of wisdom?",
+    //     ans: [
+    //         { text: "Melinoe", right: false },
+    //         { text: "Cratos", right: false },
+    //         { text: "Athena", right: true },
+    //         { text: "Iris", right: false }
+    //     ]
+    // },
+    // {
+    //     ques: "Who was the muse of love poetry?",
+    //     ans: [
+    //         { text: "Tyche", right: false },
+    //         { text: "Cupid", right: false },
+    //         { text: "Mars", right: false },
+    //         { text: "Erato", right: true }
+    //     ]
+    // },
+    // {
+    //     ques: "Who was the muse of comedy?",
+    //     ans: [
+    //         { text: "Thalia", right: true },
+    //         { text: "Diana", right: false },
+    //         { text: "Melinoe", right: false },
+    //         { text: "Tyche", right: false }
+    //     ]
+    // },
+    // {
+    //     ques: "Who was the Greek god of light and the sun?",
+    //     ans: [
+    //         { text: "Diana", right: false },
+    //         { text: "Phobus", right: false },
+    //         { text: "Apollo", right: true },
+    //         { text: "Cratos", right: false }
+    //     ]
+    // },
     {
         ques: "Who was the muse of music?",
         ans: [
@@ -101,29 +101,65 @@ let score = 0;
 function start() {
     qno = 0;
     score = 0;
-    next.innerHTML = "Next";
     showques();
 }
 
 function showques() {
     reset();
     let q = questions[qno];
-    let qnno= qno+1;
+    let qnno = qno + 1;
     question.innerHTML = qnno + ". " + q.ques;
-    q.ans.forEach(answers => {
+    q.ans.forEach(answer => {
         const btn = document.createElement("button");
-        btn.innerHTML = answers.text;
+        btn.innerHTML = answer.text;
         btn.classList.add("opt");
         option.appendChild(btn);
+        if (answer.right) {
+            btn.dataset.right = "true";
+        }
+        btn.addEventListener("click", select);
     });
 }
 
-function reset(){
-    next.style.display="none";
-    while(option.firstChild){
+function reset() {
+    while (option.firstChild) {
         option.removeChild(option.firstChild);
     }
 }
+
+function select(e) {
+    const sbtn = e.target;
+    const correct = sbtn.dataset.right === "true";
+    if (correct) {
+        sbtn.classList.add("correct");
+        score++;
+    } else {
+        sbtn.classList.add("incorrect");
+    }
+    Array.from(option.children).forEach(btn => {
+        if (btn.dataset.right === "true") {
+            btn.classList.add("correct");
+        }
+        btn.disabled = true;
+    });
+
+    if (qno < questions.length-1) {
+        next.style.display = "block";
+     } else {
+       next.innerHTML="submit";
+       next.style.display = "block";
+       next.addEventListener("click",showscore);
+     }
+    qno++;
+    function showscore()
+    {
+        question.innerHTML = "Quiz Over! Your Score: " + score + " out of " + questions.length;
+        next.style.display = "none";
+    }
+}
+next.addEventListener("click", function() {
+    showques();
+    next.style.display = "none";
+});
+
 start();
-
-
