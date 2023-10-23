45599d7071f328a2df3c9433ebd63ef999dcdc96
codage-amour <palak22154041@akgec.ac.in>
2023-10-23 21:59:35 +0530

    final 1

--- quiz.js
+++ quiz.js
@@ -1,17 +1,15 @@
 document.getElementById('submit').addEventListener('click', function() {
     const nameInput = document.getElementById('name');
     const name = nameInput.value;
-    if(name === "")
-    alert("Please enter your name");
-else
-{
     const namedisplay = document.getElementById('namedisplay');
     namedisplay.textContent =`${name}`;
     document.getElementById('main').style.display = "block";
     document.getElementById('namedisplay').style.display = "flex";
     document.getElementById('status').style.display = "block";
-    document.getElementById('enter').style.display = "none";}
+    document.getElementById('enter').style.display = "none";
+    start();
 });
+
 const questions = [
     {
         ques: "Who was the king of the gods in Greek mythology?",
@@ -104,6 +102,8 @@ const questions = [
         ]
     }
 ];
+
+
 const question = document.getElementById("question");
 const option = document.getElementById("options");
 const next = document.getElementById("next");
@@ -113,24 +113,20 @@ const time = document.getElementById("time");
 let qno = 0;
 let score = 0;
 let timerInterval;
-let selectedAnswers = new Array(questions.length).fill(null);
+//let selectedOption = [];
+//let submitted = false;
 
-const buttons = document.querySelectorAll('.n');
-buttons.forEach(button => {
-    button.addEventListener('click', function() {
-        const questionNumber = this.getAttribute('data-question');
-        qno = parseInt(questionNumber) - 1;
-        showques();
-    });
-});
+
 
 function start() {
     qno = 0;
+    //let totalScore = 0;
+    //selectedOptions = Array(questions.length).fill(-1);
+    //submitted = false; 
+    showques();
     time.style.display="block";
-    next.style.display="block";
     sub.style.display="block";
     end.style.display="block";
-    option.style.display="block";
     let timeLeft = 300;
     function formatTime(seconds) {
         const minutes = Math.floor(seconds / 60);
@@ -145,12 +141,11 @@ function start() {
         time.textContent = "Time left: " + formatTime(timeLeft);
         
         if (timeLeft === 0) {
-            // clearInterval(timerInterval);
-            // time.style.display = "none";
-            show();
+            clearInterval(timerInterval);
+            time.style.display = "none";
+            showscore();
         }
     }, 1000);
-    showques();
 }
 
 function showques() {
@@ -162,27 +157,25 @@ function showques() {
     let q = questions[qno];
     let qnno = qno + 1;
     question.innerHTML = qnno + ". " + q.ques;
-
+    
     q.ans.forEach((answer, index) => {
         const btn = document.createElement("button");
         btn.innerHTML = answer.text;
         btn.classList.add("opt");
-    
-        if (selectedAnswers[qno] === index) {
-            btn.classList.add("select");
-        }
-
-        btn.addEventListener("click", function () {
-
-            selectedAnswers[qno] = index;
-            resetButtonColors();
-            this.classList.add("select");
-        });
-
         option.appendChild(btn);
-    });
+        if (answer.right) {
+            btn.dataset.right = "true";
+        }
+        
+    //     if (!submitted) {
+    //         btn.addEventListener("click", () => select(index));
+    //     }
+        
+    //     if (selectedOption === index) {
+    //         btn.classList.add("select");
+    //     }
+});
 }
-
 
 function reset() {
     while (option.firstChild) {
@@ -190,83 +183,72 @@ function reset() {
     }
 }
 
-function resetButtonColors() {
-    const buttons = document.querySelectorAll('.opt');
-    buttons.forEach(button => {
-        button.classList.remove("select");
-    });
-}
+// function select(selectedIndex) {
+//     if (!submitted) {
+//         if (selectedOptions[qno] !== -1) {
+//             option.children[selectedOptions[qno]].classList.remove("select");
+//         }
+//         selectedOptions[qno] = selectedIndex;
+//         const selectedBtn = option.children[selectedIndex];
+//         selectedBtn.classList.add("select");
+//     }
+// }
 
-function resetNavigationButtons() {
-    const buttons = document.querySelectorAll('.n');
-    buttons.forEach(button => {
-        button.style.backgroundColor = ""; // Reset button color
-    });
-}
+
+// function calculateScore() {
+//     for (let i = 0; i < questions.length; i++) {
+//         if (selectedOptions[i] !== -1) {
+//             if (questions[i].ans[selectedOptions[i]].right) {
+//                 totalScore += 4; 
+//             } else {
+//                 totalScore -= 1; // Incorrect answer
+//             }
+//         }
+//     }
+// }
+
+// function show() {
+//     clearInterval(timerInterval); // Stop the timer
+//     resetButtonColors();
+//     resetNavigationButtons();
+//     submitted = false; // Reset the submitted flag
+//     qno = 0; // Reset the question number
+//     calculateScore(); // Calculate the user's score
+//     question.innerHTML = `Quiz Over! Your Score: ${totalScore} out of ${questions.length * 4}`;
+//     next.innerHTML = "Re-Attempt";
+//     time.style.display = "none";
+//     sub.style.display = "none";
+//     end.style.display = "none";
+//     // Handle the "Re-Attempt" button to restart the quiz
+//     next.addEventListener("click", function () {
+//         start();
+//     });
+// }
+
 
 next.addEventListener("click", function () {
-    // if (!submitted) {
+    if (!submitted) {
         if (qno < questions.length - 1) {
             qno++;
             showques();
         } else {
-          next.style.display="none";
+            showques();
         }
-    // }
+    }
     const nextButtonId = `q${qno}`;
     const nextButton = document.getElementById(nextButtonId);
     nextButton.style.backgroundColor = "purple";
 });
 
-sub.addEventListener("click", function () {
-    // if (!submitted) {
-        if (qno < questions.length - 1) {
-            qno++;
-            showques();
-        } else {
-            showques();
-        }
-    // }
-    calculate();
-    const nextButtonId = `q${qno}`;
-    const nextButton = document.getElementById(nextButtonId);
-    nextButton.style.backgroundColor = "green";
-});
+// sub.addEventListener("click",function(){
+//     if (!submitted) {
+//         submitted = true; // Mark the quiz as submitted
+//             calculateScore();
+//         }
+//     const nextButtonId = `q${qno}`;
+//     const nextButton = document.getElementById(nextButtonId);
+//     nextButton.style.backgroundColor = "green";
+// })
 
-function calculate() {
-    const currentQuestion = questions[qno];
-    const selectedAnswerIndex = selectedAnswers[qno];
-
-    if (selectedAnswerIndex !== null) {
-        const selectedAnswer = currentQuestion.ans[selectedAnswerIndex];
-
-        if (selectedAnswer.right) {
-            score += 4;
-        } else{
-            score -= 1;
-        }
-    }
-}
-
-end.addEventListener("click",show);
-
-function show() {
-    clearInterval(timerInterval);
-    calculate();
-    question.innerHTML = `Quiz Over! Your Score: ${score} out of ${questions.length * 4}`;
-
-    next.innerHTML = "Re-Attempt";
-    next.style.display = "block";
-    time.style.display = "none";
-    sub.style.display = "none";
-    end.style.display = "none";
-    option.style.display = "none";
-
-    next.addEventListener("click", function () {
-        selectedAnswers[qno] = null;
-        resetButtonColors();
-        resetNavigationButtons();
-        start();
-    });
-}
+// end.addEventListener("click",show);
 start();
